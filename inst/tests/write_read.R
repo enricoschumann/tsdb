93c55e13e22f28998a4577a4960cf6ea4f7a35d0
Enrico Schumann <es@enricoschumann.net>
2024-11-05 16:57:35 +0100

    Clarify messages

--- inst/tests/write_read.R
+++ inst/tests/write_read.R
@@ -1,11 +1,10 @@
 ## write nf time-series to files, then read/merge them
 
-nf <- 100    ## number of files
+nf <- 100     ## number of files
 nd <- 250*25  ## number of days per time-series (~25 years)
 
-
-
 ## ----------------------------------------------
+
 library("tsdb")
 library("zoo")
 
@@ -18,8 +17,8 @@ trials <- 10
 f <- function(s)
     format(s, width = 31)
 
-message("\nRun ", trials, " trials:")
-message("  ", nf, " files of length ", nd, "\n")
+message("\nRun timing tests:")
+message("  write and read/merge ", nf, " files of length ", nd, "\n")
 
 
 message(f("write files (from zoo)"), appendLF = FALSE)
@@ -31,7 +30,8 @@ t <- system.time(
                            file = i, dir = d, replace.file = TRUE)}
     }
 )
-message(round(t[[3]]/trials, 2), " seconds")
+message(round(t[[3]]/trials, 2),
+        " seconds (averaged over ", trials, " trials)")
 
 
 
@@ -39,12 +39,13 @@ message(round(t[[3]]/trials, 2), " seconds")
 message(f("read/merge 10 years"), appendLF = FALSE)
 t <- system.time(
     for (i in 1:trials ) {
-    read_ts_tables(as.character(1:nf), dir = d,
-                   start = "2007-1-1", end = "2016-12-31",
-                   return.class = "zoo", column.names = "%file%")
+        S1 <- read_ts_tables(as.character(1:nf), dir = d,
+                             start = "2007-1-1", end = "2016-12-31",
+                             return.class = "zoo", column.names = "%file%")
     }
 )
-message(round(t[[3]]/trials, 2), " seconds")
+message(round(t[[3]]/trials, 2),
+        " seconds (averaged over ", trials, " trials)")
 
 
 
@@ -52,13 +53,14 @@ message(round(t[[3]]/trials, 2), " seconds")
 message(f("(f)read/merge 10 years"), appendLF = FALSE)
 t <- system.time(
     for (i in 1:trials ) {
-    read_ts_tables(as.character(1:nf), dir = d,
-                   start = "2007-1-1", end = "2016-12-31",
-                   return.class = "zoo", column.names = "%file%",
-                   read.fn = "fread")
+        S2 <- read_ts_tables(as.character(1:nf), dir = d,
+                             start = "2007-1-1", end = "2016-12-31",
+                             return.class = "zoo", column.names = "%file%",
+                             read.fn = "fread")
     }
 )
-message(round(t[[3]]/trials, 2), " seconds")
+message(round(t[[3]]/trials, 2),
+        " seconds (averaged over ", trials, " trials)")
 
 
 
@@ -66,11 +68,12 @@ message(round(t[[3]]/trials, 2), " seconds")
 message(f("read/merge complete series"), appendLF = FALSE)
 t <- system.time(
     for (i in 1:trials ) {
-    read_ts_tables(as.character(1:nf), dir = d,
-                   return.class = "zoo", column.names = "%file%")
+        read_ts_tables(as.character(1:nf), dir = d,
+                       return.class = "zoo", column.names = "%file%")
     }
 )
-message(round(t[[3]]/trials, 2), " seconds")
+message(round(t[[3]]/trials, 2),
+        " seconds (averaged over ", trials, " trials)")
 
 
 
@@ -78,42 +81,48 @@ message(round(t[[3]]/trials, 2), " seconds")
 message(f("(f)read/merge complete series"), appendLF = FALSE)
 t <- system.time(
     for (i in 1:trials ) {
-    read_ts_tables(as.character(1:nf), dir = d,
-                   return.class = "zoo", column.names = "%file%",
-                   read.fn = "fread")
+        read_ts_tables(as.character(1:nf), dir = d,
+                       return.class = "zoo", column.names = "%file%",
+                       read.fn = "fread")
     }
 )
-message(round(t[[3]]/trials, 2), " seconds")
+message(round(t[[3]]/trials, 2),
+        " seconds (averaged over ", trials, " trials)")
 
 
+## ----------------------------------------------
 
-x <- 1:1000000
+
+nd <- 1e6
+
+message("\nRun timing tests:")
+message("  write and read a file of length ", nd, "\n")
+
+x <- seq_len(nd)
 z <- zoo(x, as.POSIXct("2001-1-1") + x)
 write_ts_table(as.ts_table(z, columns = "A"),
                file = "single", dir = d, replace.file = TRUE)
-message("\nRun ", trials, " trials:")
-message("  ", 1, " file of length ", length(x), "\n")
 
 message(f("read a single file"), appendLF = FALSE)
 t <- system.time(
     for (i in 1:trials ) {
         ignore <- read_ts_tables("single", dir = d,
-                       start = min(index(z)), end = max(index(z)),
-                       return.class = "zoo", column.names = "%file%")
+                                 start = min(index(z)), end = max(index(z)),
+                                 return.class = "zoo", column.names = "%file%")
     }
 )
-message(round(t[[3]]/trials, 2), " seconds")
+message(round(t[[3]]/trials, 2),
+        " seconds (averaged over ", trials, " trials)")
 
-## ---------------------------------------------------
 
 message(f("(f)read a single file"), appendLF = FALSE)
 t <- system.time(
     for (i in 1:trials ) {
         ignore <- read_ts_tables("single", dir = d,
-                       start = min(index(z)), end = max(index(z)),
-                       return.class = "zoo", column.names = "%file%",
-                       read.fn = "fread")
+                                 start = min(index(z)), end = max(index(z)),
+                                 return.class = "zoo", column.names = "%file%",
+                                 read.fn = "fread")
     }
 )
-message(round(t[[3]]/trials, 2), " seconds")
-
+message(round(t[[3]]/trials, 2),
+        " seconds (averaged over ", trials, " trials)")
