49204f3fa7986598d64d5f9fa36861bc28f2dfa0
Enrico Schumann <es@enricoschumann.net>
2025-02-20 11:10:10 +0100

    Suppress coloured output
    When tests are run non-interactively, suppress
    coloured output.

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,3 +1,4 @@
-if (requireNamespace("tinytest", quietly = TRUE)) {
-    tinytest::test_package("tsdb")
-}
+if (requireNamespace("tinytest", quietly = TRUE))
+    tinytest.results <- tinytest::test_package("tsdb",
+                                               color = interactive(),
+                                               verbose = 1)
